d0b848285b2f4f60316db1ba83928de520dec402
CODECZERO <as8857981@gmail.com>
2024-08-08 19:50:22 +0530

    added update user data options

---
+++ server/src/util/fileUploder.util.ts
@@ -0,0 +1,25 @@
+import { v2 as fileServer} from "cloudinary";
+import fs from "fs";
+fileServer.config({//config key for fileServer to access the server
+    cloud_name:process.env.CLOUDNAME,
+    api_key:process.env.CLOUDAPIKEY,
+    api_secret:process.env.CLOUDAPISECRET
+})
+
+//this function is going to upload file on server but before that we are going to store it on local server and then upload it to cloud server
+const uploadFile=async(localFilePath:any)=>{
+    try {
+        if(!localFilePath)return null;//checking if file path is therer
+        const upload=await fileServer.uploader.upload(localFilePath,{//uploading it on server
+            resource_type:"auto"
+        });
+        fs.unlinkSync(localFilePath);//removing it from local file
+        return upload;//reutnring it's link
+    } catch (error) {
+        fs.unlinkSync(localFilePath);
+        return null;
+    }
+}
+
+export {uploadFile}
+
