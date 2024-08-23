c773c15917182b9382f1e4456cd423121efcee04
CODECZERO <as8857981@gmail.com>
2024-08-23 07:20:36 +0530

    improved chat serivce

--- server/src/middelware/chatConnect.middeleware.ts
+++ server/src/middelware/chatConnect.middeleware.ts
@@ -1,8 +1,26 @@
-import { NextFunction } from "express";
-import runWebSocket from "../services/chat/chat.service";
-import { run } from "node:test";
+import { Request, Response, NextFunction } from "express";
+import AsyncHandler from "../util/ayscHandler.js";
+import { ApiError } from "../util/apiError.js";
+import { WebSocket } from "ws";
 
-const connectChat=async(req:Request,res:Response,next:NextFunction)=>{
-    const roomName=req.url.valueOf();
-    await runWebSocket(roomName);
-}
+const connectChat = AsyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const roomNameUrl = req.url.valueOf();
+    if (!roomNameUrl) throw new ApiError(400, "room name is not provided");
+    const ws = new WebSocket("ws://localhost:9000");
+    ws.onopen = () => {
+        // Room name could be set here or retrieved from some UI input
+        const roomName = roomNameUrl;
+        const message = "Hello, room!";
+
+        // Sending message along with the room name
+        ws.send(JSON.stringify({ roomName, message }));
+    };
+
+    ws.onmessage = (event) => {
+        const data = JSON.parse(event.data as string);
+        console.log("Received:", data.message);
+    };
+
+});
+
+export default connectChat;
--- server/src/router/user.router.ts
+++ server/src/router/user.router.ts
@@ -2,12 +2,13 @@ import { Router } from "express";
 import { signup, login, getCollege,updatePassword } from "../controller/user.controller.js";
 import { verifyData } from "../middelware/auth.middeleware.js";
 import runWebSocket from "../services/chat/chat.service.js";
+import connectChat from "../middelware/chatConnect.middeleware.js";
 
 const router = Router();
 
 router.route("/signup").post(signup).get(getCollege);
 router.route("/login").post(login);
 router.route("/userData").put(verifyData,updatePassword);
-router.route("/chat/:college/:branch").get(runWebSocket);
+router.route("/chat/:college/:branch").put(connectChat);
 
 export default router;
