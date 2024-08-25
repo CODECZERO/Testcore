ab3ef887800b1ca85c54be8f318d48a180bf93ea
CODECZERO <as8857981@gmail.com>
2024-08-25 18:10:27 +0530

    folder change

--- server/dist/router/user.router.js
+++ server/dist/router/user.router.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { signup, login, getCollege, updatePassword } from "../controller/user.controller.js";
 import { verifyData } from "../middelware/auth.middeleware.js";
+// import connectChat from "../middelware/chatConnect.middeleware.js";
 const router = Router();
 router.route("/signup").post(signup).get(getCollege);
 router.route("/login").post(login);
 router.route("/userData").put(verifyData, updatePassword);
+// router.route("/chat/:college/:branch").get(connectChat);
 export default router;
