88c00f7d53c65306024896d50d6c75b18fc4e917
CODECZERO <as8857981@gmail.com>
2024-08-27 17:55:30 +0530

    changes in index

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,7 +3,7 @@ config();
 import { connectDb } from "./db/database.MongoDb.js";
 import app from "./app.js";
 connectDb().then(() => {
-    app.listen(process.env.PORT || 8080, () => { console.log(`App running on port:${process.env.PORT}`) });
+    app.listen(process.env.PORT || 8888|| 3000, () => { console.log(`App running on port:${process.env.PORT}`) });
    
 }).catch((error) => {
     console.log(`Database connection fail ${error}`);
