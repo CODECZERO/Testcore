116abcf5480f20ba3273706ed18b136c0e4a5a2f
CODECZERO <as8857981@gmail.com>
2024-08-27 17:27:10 +0530

    changes in dist

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,10 +2,9 @@ import { config } from "dotenv";
 config();
 import { connectDb } from "./db/database.MongoDb.js";
 import app from "./app.js";
-import runWebSocket from "./services/chat/chat.service.js";
 connectDb().then(() => {
     app.listen(process.env.PORT || 8080, () => { console.log(`App running on port:${process.env.PORT}`) });
-    runWebSocket();
+   
 }).catch((error) => {
     console.log(`Database connection fail ${error}`);
 })
