a48cb40c8d757338fff8ee8e0d4fee4097640146
CODECZERO <as8857981@gmail.com>
2024-09-17 18:05:00 +0530

    added routers

--- server/dist/router/chat.router.js
+++ server/dist/router/chat.router.js
@@ -1,10 +1,10 @@
-import { LeaveRoom, connectChat, createChatRoom, deleteChat, getUserInChat, joinChatRoom } from "../controller/chat.controller.js";
+import { LeaveRoom, connectChat, createChatRoom, getUserInChat, joinChatRoom } from "../controller/chat.controller.js";
 import { verifyData } from "../middelware/auth.middeleware.js";
 import { SearchChatRoom } from "../middelware/chat.middeleware.js";
 import { Router } from "express";
 const router = Router();
 router.route("/createChat").post(verifyData, createChatRoom);
 router.route("/ChatQuery/:College/:Branch").post(verifyData, joinChatRoom).
-    put(verifyData, SearchChatRoom, LeaveRoom).delete(verifyData, deleteChat).get(verifyData, SearchChatRoom, getUserInChat);
+    put(verifyData, SearchChatRoom, LeaveRoom).get(verifyData, SearchChatRoom, getUserInChat);
 router.route("/connectChat/:College/:Branch").post(verifyData, SearchChatRoom, connectChat);
 export default router;
