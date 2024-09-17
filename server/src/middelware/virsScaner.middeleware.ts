b3d1e3fc0c796db5f444d3f062ddfd01ce07b115
CODECZERO <as8857981@gmail.com>
2024-09-17 16:21:08 +0530

    added routers

--- server/src/middelware/virsScaner.middeleware.ts
+++ server/src/middelware/virsScaner.middeleware.ts
@@ -9,13 +9,13 @@ interface CustomRequest extends Request {
     filePath?: string;
 }
 
-export const ScanForVirus = AsyncHandler(async (req: CustomRequest, res: Response, next: NextFunction) => {
-    const filePath = req.filePath;
+export const ScanForVirus = AsyncHandler(async (req: CustomRequest, res: Response, next: NextFunction) => {//this function scans the file which are uploaded on the server
+    const filePath = req.filePath;//takes file path 
     if (!filePath) throw new ApiError(400, "File address not provided");
 
-    const scanResult= await scanFile(filePath);
-    if(!scanResult) next();
-    await fs.promises.unlink(filePath)
-    res.status(400).send({ message: 'Malware detected' });
+    const scanResult= await scanFile(filePath);//put the file through virus scan
+    if(!scanResult) next();//if there is no result then it means , it's malaware free
+    await fs.promises.unlink(filePath)//else remove the file from server
+    res.status(400).send({ message: 'Malware detected' });//and not uploaded to cloud
     next()
 });
