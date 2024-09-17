30a6e69f88798b24aa1edcd4308f85722e2234bb
CODECZERO <as8857981@gmail.com>
2024-09-17 16:21:08 +0530

    added routers

--- server/src/middelware/fileUpload.middelware.ts
+++ server/src/middelware/fileUpload.middelware.ts
@@ -1,5 +1,7 @@
 import multer from "multer";
 
+
+//multer fucntion to store file in local server
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, "./public/temp")
