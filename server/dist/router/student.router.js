c7bf9eb3a45b2676f798e132dc828480a3eb7fcb
CODECZERO <as8857981@gmail.com>
2024-10-22 19:54:22 +0530

    changed in student logic

--- server/dist/router/student.router.js
+++ server/dist/router/student.router.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { getExam, getQuestionPaperForStundet, getResult, getTimeTable, giveExam } from "../controller/student.controller.js";
+import { verifyData } from "../middelware/auth.middeleware.js";
 const router = Router();
-router.route("/Exam").get(getExam).put(giveExam);
+router.route("/Exam").get(verifyData, getExam).put(verifyData, giveExam);
 router.route("/TimeTalbe").put(getTimeTable);
 router.route("/Result").get(getResult);
 router.route("/Question").get(getQuestionPaperForStundet);
