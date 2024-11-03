0794d1332afa0da923591791d931670fe71bafbb
CODECZERO <as8857981@gmail.com>
2024-11-03 11:33:05 +0530

    added stress testing and scaling

--- server/src/db/database.MongoDb.ts
+++ server/src/db/database.MongoDb.ts
@@ -9,7 +9,7 @@ interface ExamDocument {
 const connectDb = async () => {//connects data base and handle error
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}`);
-        return "connected";
+        return connectionInstance;
     } catch (error) {
         console.log(`There is error while connecting to db \n \t ${error} `)
         process.exit(1);
@@ -27,4 +27,15 @@ const searchMongodb = async (tokenID: string): Promise<ExamDocument | null> => {
     }
 };
 
-export { connectDb, searchMongodb };
+const closeDb = async (client:any) => {//this function close mongodb database connection
+    try {
+        await client.close();
+        console.log("Disconnected from MongoDB");
+        process.exit(0);
+    } catch (error) {
+        console.log(`something went wrong while closeing mongodb connection ${error}`);
+    }
+}
+
+
+export { connectDb, searchMongodb, closeDb };
