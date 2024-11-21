debb4160cdaaa7d896dcafa2cc04676ed5f8b278
CODECZERO <as8857981@gmail.com>
2024-11-21 10:21:24 +0530

    update docker file

--- server/dist/db/role.db.js
+++
@@ -1,9 +0,0 @@
-import prisma from "./database.Postgres.js";
-// Define the mapping between user roles and Prisma models
-const roleToModel = {
-    Student: prisma.student,
-    College: prisma.college,
-    Examiner: prisma.examiner, // Prisma model for examiners
-    // Add other roles as needed
-};
-export default roleToModel;
