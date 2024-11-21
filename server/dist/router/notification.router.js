dd68eee96afea1121dfedbb1f3cd8aa7b5aae327
CODECZERO <as8857981@gmail.com>
2024-11-21 08:56:43 +0530

    added docker file

--- server/dist/router/notification.router.js
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-import { messageHandler } from "../controller/notification.controller.js";
-const router = Router();
-router.route("/sendNotification").post(messageHandler);
-export default router;
--- server/dist/router/student.router.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getExam, getQuestionPaperForStundet, getResult, getTimeTable, giveExam } from "../controller/student.controller.js";
-import { verifyexamData } from "../middelware/auth.middeleware.js";
-const router = Router();
-router.route("/Exam").get(verifyexamData, getExam).put(verifyexamData, giveExam);
-router.route("/TimeTalbe").put(getTimeTable);
-router.route("/Result").get(verifyexamData, getResult);
-router.route("/Question").get(verifyexamData, getQuestionPaperForStundet);
-export default router;
