6fed2451bda00ab75a94992f301694f4e8565806
CODECZERO <as8857981@gmail.com>
2024-11-21 10:21:24 +0530

    update docker file

--- server/dist/index.js
+++
@@ -1,9 +0,0 @@
-import { config } from "dotenv";
-config();
-import app from "./app.js";
-import connectAll from "./util/appStarupt.util.js";
-connectAll().then(() => {
-    app.listen(process.env.PORT || 8080, () => { console.log(`App running on port:${process.env.PORTMAIN}`); });
-}).catch((error) => {
-    console.log(`Database connection fail ${error}`);
-});
--- server/dist/services/twilio/twilioClinet.service.js
+++
@@ -1,5 +0,0 @@
-import twilio from "twilio";
-const AccountSid = process.env.TWILIO_ACCOUNT_SID;
-const AuthToken = process.env.TWILIO_ACCOUNT_AUTHTOKEN;
-const clinet = twilio(AccountSid, AuthToken);
-export { clinet };
