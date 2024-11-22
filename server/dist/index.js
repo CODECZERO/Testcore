5abf2cd17e30f4f47bcd36fd22dce228b7cf32b5
CODECZERO <as8857981@gmail.com>
2024-11-22 07:51:07 +0530

    update

--- server/dist/index.js
+++ server/dist/index.js
@@ -3,7 +3,7 @@ config();
 import app from "./app.js";
 import connectAll from "./util/appStarupt.util.js";
 connectAll().then(() => {
-    app.listen(process.env.PORT || 8080, () => { console.log(`App running on port:${process.env.PORT}`); });
+    app.listen(4008, () => { console.log(`App running on port:${4008}`); });
 }).catch((error) => {
     console.log(`Database connection fail ${error}`);
 });
