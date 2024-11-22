b502ff9f0c77bb8ebe1178bcfe3e15be273d635b
CODECZERO <as8857981@gmail.com>
2024-11-22 10:42:18 +0530

    update

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,7 +5,7 @@ import app from "./app.js";
 import connectAll from "./util/appStarupt.util.js";
 
 connectAll().then(() => {
-    app.listen(process.env.PORT || 4008, () => { console.log(`App running on port:${process.env.PORT||4008}`) });
+    app.listen(process.env.PORTMAIN || 4008, () => { console.log(`App running on port:${process.env.PORTMAIN||4008}`) });
 }).catch((error) => {
     console.log(`Database connection fail ${error}`);
 })
