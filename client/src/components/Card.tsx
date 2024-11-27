98f037d5ada82b126b7fb98747a64534db4879ce
Gaurav <gauravdpatil06@gmail.com>
2024-11-27 13:24:29 +0530

    Added profile

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import '../styles/card.css';
-
+import '../styles/Card.css'
 interface CardProps {
   title: string;
   description: string;
   icon: React.ReactNode;
+
 }
 
-const Card: React.FC<CardProps> = ({ title, description, icon }) => {
+const Card: React.FC<CardProps> = ({ title, description, icon}) => {
   return (
-    <div className="card">
+    <div className="card" > {/* Use 'onClick' correctly */}
       <div className="card-icon">{icon}</div>
       <h3 className="card-title">{title}</h3>
       <p className="card-description">{description}</p>
