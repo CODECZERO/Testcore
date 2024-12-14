f5e6d9dd0525d6e6e15acbbaae6f27b906197b62
CODECZERO <as8857981@gmail.com>
2024-12-14 11:47:07 +0530

    solved echo bug

--- server/dist/services/chat/chatMethodes.services.js
+++ server/dist/services/chat/chatMethodes.services.js
@@ -7,89 +7,67 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-import { parse } from "url";
-import { clients, rooms } from './chatServer.service.js';
+import { rooms } from './chatServer.service.js';
 import { ApiError } from "../../util/apiError.js";
 import rabbitmq from "../rabbitmq/rabbitmq.services.js";
-import { ChatTokenDec } from "./chatToken.services.js";
-const sendMessage = (MessageData, ws) => __awaiter(void 0, void 0, void 0, function* () {
+const sendMessage = (messageData, ws) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const messageInfo = JSON.stringify(MessageData); //converts message pattern to string or json to string
-        // const messageEnc=await SendMessageEncryption();//it a enctyption function, which encrypts message bfore sending,it to queue
-        //so only the user/group/group member person can only open/read that message
-        rabbitmq.publishData(messageInfo, MessageData.roomName); //publishing or send data to rabbitmq queue, so it can make record of message for 7 day
-        //or for user defin time and it's a way of scaling the whole chat feature/application, insted of rabbitmq you can use kafka as it will be fast and quit robust
+        const messageInfo = JSON.stringify(messageData);
+        yield rabbitmq.publishData(messageInfo, messageData.roomName);
     }
     catch (error) {
-        throw new ApiError(500, "error while sending message"); //throw error if any thing went wrong, so later the dev can debug it 
+        console.error("Error while sending message:", error);
+        throw new ApiError(500, "Error while sending message");
     }
 });
-const sendMessageToReciver = (message, ws) => __awaiter(void 0, void 0, void 0, function* () {
-    //message to other and help theme recive that message also
+const broadcastMessage = (message, roomName) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const messageContent = message.content.toString();
         const parsedMessage = JSON.parse(messageContent);
-        for (const client of clients) {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(messageContent);
+        if (rooms[roomName]) {
+            for (const client of rooms[roomName]) {
+                if (client.readyState === WebSocket.OPEN && client.userId !== parsedMessage.userId) {
+                    client.send(messageContent);
+                }
             }
         }
     }
     catch (error) {
-        console.error("Error while sending message to receiver:", error);
-        throw new ApiError(500, "Error while receiving message");
+        console.error("Error while broadcasting message:", error);
+        throw new ApiError(500, "Error while broadcasting message");
     }
 });
-const reciveMEssage = (roomName, ws) => __awaiter(void 0, void 0, void 0, function* () {
+const receiveMessage = (ws) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        yield rabbitmq.subData(roomName); //subscribe to the queue, the queue name is same as roomName 
+        if (!ws.roomName) {
+            throw new ApiError(400, "Room name not set for WebSocket");
+        }
+        yield rabbitmq.subData(ws.roomName);
         yield rabbitmq.channel.consume(rabbitmq.queue.queue, (message) => {
-            //send it to user 
-            if (message)
-                sendMessageToReciver(message, ws).catch(console.error);
+            if (message) {
+                broadcastMessage(message, ws.roomName).catch(console.error);
+                rabbitmq.channel.ack(message);
+            }
         });
     }
     catch (error) {
-        throw new ApiError(500, "error while reciveing message"); //throw error if any thing went wrong, so later the dev can debug it 
+        console.error("Error while receiving message:", error);
+        throw new ApiError(500, "Error while receiving message");
     }
 });
-const closeSocket = (MessageData, ws) => __awaiter(void 0, void 0, void 0, function* () {
+const closeSocket = (messageData, ws) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        rooms[MessageData.roomName].delete(ws); //checks the roomName in the collection of the rooms and removes the websocket connection from that room
-        //rooms is a object which has information about how many users are connected to server as any user close's or end the connection with server
-        //it will remove user from object 
-        if (rooms[MessageData.roomName].size === 0) { //if there is no one in the current websocket server of that room or user of a room are not connected to the
-            //websocket, it will delete the room from the rooms(object of room);.
-            delete rooms[MessageData.roomName];
+        if (messageData.roomName && rooms[messageData.roomName]) {
+            rooms[messageData.roomName].delete(ws);
+            if (rooms[messageData.roomName].size === 0) {
+                delete rooms[messageData.roomName];
+            }
         }
+        ws.close();
     }
     catch (error) {
         console.error("Error while closing socket:", error);
         throw new ApiError(500, "Error while closing socket");
     }
 });
-const closeConnection = () => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        yield rabbitmq.closeConnection();
-    }
-    catch (error) {
-        throw new ApiError(500, "error while closeing connection"); //throw error if any thing went wrong, so later the dev can debug it 
-    }
-});
-const tokenExtractr = (req) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        const parsedUrl = parse(req.url || " ", true); //it parse query as string
-        const queryParams = parsedUrl.query; //takes query from parsedUrl and  
-        const tokenFromUser = queryParams === null || queryParams === void 0 ? void 0 : queryParams.token;
-        if (!tokenFromUser)
-            throw new ApiError(400, "bad request data is not provided");
-        const chatToken = yield ChatTokenDec(tokenFromUser);
-        if (chatToken instanceof ApiError)
-            throw new ApiError(500, "something went wrong while decodeing token");
-        return chatToken;
-    }
-    catch (error) {
-        throw new ApiError(500, `Someting went wrong while extracing token error: ${error}`);
-    }
-});
-export { sendMessage, reciveMEssage, closeSocket, closeConnection, tokenExtractr, };
+export { sendMessage, receiveMessage, closeSocket, };
