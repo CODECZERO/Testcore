e6eed046de13b1c6a61b5ce59e367b7ea6b0c7ca
CODECZERO <as8857981@gmail.com>
2024-12-15 14:09:37 +0530

    image upload

--- server/dist/router/user.router.js
+++ server/dist/router/user.router.js
@@ -1,12 +1,18 @@
 import { Router } from "express";
-import { signup, login, getCollege, updatePassword, SessionActive, getClass, createClass } from "../controller/user.controller.js";
+import { signup, login, getCollege, updatePassword, SessionActive, getClass, createClass, updateProfileImage } from "../controller/user.controller.js";
 import { verifyData } from "../middelware/auth.middeleware.js";
 import validateAndSanitize from "../middelware/security.middeleware.js";
+import { upload } from "../middelware/fileUpload.middelware.js";
 // import connectChat from "../middelware/chatConnect.middeleware.js";
 const router = Router();
 router.route("/signup").post(validateAndSanitize, signup).get(getCollege);
 router.route("/login").post(login);
-router.route("/userData").put(verifyData, updatePassword).get(verifyData, SessionActive);
+router.route("/userData").put(verifyData, updatePassword).get(verifyData, SessionActive).post(verifyData, upload.fields([
+    {
+        name: "ProfileImage",
+        maxCount: 1
+    },
+]), updateProfileImage);
 router.route("/class").get(getClass).post(createClass);
 // router.route("/chat/:college/:branch").get(connectChat);
 export default router;
