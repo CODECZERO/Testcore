6bcd483940a9c28db65ad8d7acfdae4116f51886
CODECZERO <as8857981@gmail.com>
2024-12-25 12:23:55 +0530

    chat room finding error solve

--- server/src/middelware/chat.middeleware.ts
+++ server/src/middelware/chat.middeleware.ts
@@ -10,7 +10,7 @@ interface Requestany extends Request {
 
 
 const SearchChatRoom = AsyncHandler(async (req: Requestany, res: Response, next: NextFunction) => {//this function check if chat room exists for that college and branch
-    const roomID = await cacheSearchForChatRoom(req.params.roomName[0]);//check for chat room in chace
+    const roomID = await cacheSearchForChatRoom(req.params.roomName);//check for chat room in chace
     console.log(roomID);
     if (!(roomID)) throw new ApiError(404, "chat room not found, make sure it's register");
     const roomName = req.params.roomName;//room name would be collegeName/branch
