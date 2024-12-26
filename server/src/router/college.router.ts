1498c647a74cdd57604acddc109f3d7ab1691478
CODECZERO <as8857981@gmail.com>
2024-12-26 18:51:13 +0530

    change in http methode of college

--- server/src/router/college.router.ts
+++ server/src/router/college.router.ts
@@ -3,8 +3,8 @@ import { verifyData } from "../middelware/auth.middeleware.js";
 import { AprroveTimeTable, CreateSubject, StudentVeryify, TimeTableSearch, findStudnets, getExaminer, getSubjects } from "../controller/college.controller.js";
 const router=Router();
 
-router.route("/subject").get(verifyData,TimeTableSearch).put(getSubjects).put(AprroveTimeTable)
-router.route("/createSubject").put(CreateSubject);
+router.route("/subject").get(verifyData,TimeTableSearch).post(getSubjects).put(AprroveTimeTable)
+router.route("/createSubject").post(CreateSubject);
 router.route("/student").get(verifyData,findStudnets).put(verifyData,StudentVeryify);
 router.route("/examiner").get(verifyData,getExaminer);
 
