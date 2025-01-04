8dea04aced63042f3added092cc86fe45231e6a7
CODECZERO <84948522+CODECZERO@users.noreply.github.com>
2025-01-04 19:28:05 +0530

    Update fileUpload.middelware.ts

--- server/src/middelware/fileUpload.middelware.ts
+++ server/src/middelware/fileUpload.middelware.ts
@@ -1,10 +1,26 @@
 import multer from "multer";
+import path from "path";
+import fs from "fs";
 
 
+//a function to ensure that folder exist 
+const ensureFolder = async (filePath: string) => {
+  try {
+    if (fs.existsSync(filePath)) {
+      return true;
+    }
+    await fs.promises.mkdir(filePath, { recursive: true });
+    return true;
+  } catch (error) {
+    return error;
+  }
+}
 //multer fucntion to store file in local server
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "../../public");
+    const folderExistPath = path.resolve(path.dirname("../public/images"));
+    ensureFolder(folderExistPath);
+    cb(null, folderExistPath);
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
