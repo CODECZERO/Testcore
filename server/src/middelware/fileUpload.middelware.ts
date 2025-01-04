ed6e691ee76dc26c3dead5b3df4b92c1bb0afa98
CODECZERO <84948522+CODECZERO@users.noreply.github.com>
2025-01-04 20:00:03 +0530

    Update fileUpload.middelware.ts
    I am doing test in production, so remember one thing never do this

--- server/src/middelware/fileUpload.middelware.ts
+++ server/src/middelware/fileUpload.middelware.ts
@@ -20,7 +20,8 @@ const ensureFolder = async (filePath: string) => {
 const storage = multer.diskStorage({
   destination: async function (req, file, cb) {
     try {
-      const folderExistPath = path.resolve(__dirname, './public');
+      const folderExistPath = path.resolve(__dirname, '.');
+      console.log(folderExistPath)
       await ensureFolder(folderExistPath);
       console.log("Resolved image path:", folderExistPath); // Log the resolved path
       cb(null, folderExistPath);
