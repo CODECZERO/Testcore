0613e160d8312f3bdf9beb684470ebfcfc6bf708
CODECZERO <84948522+CODECZERO@users.noreply.github.com>
2025-10-03 16:24:22 +0530

    Update ApproveTimetable.tsx

--- client/src/components/College/ApproveTimetable.tsx
+++ client/src/components/College/ApproveTimetable.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const BackendUrl = 'https://testcore-qmyu.onrender.com';
+const BackendUrl = 'https://testcore-3en7.onrender.com';
 
 const ApproveTimetable = () => {
     const [timetableId, setTimetableId] = useState('');
