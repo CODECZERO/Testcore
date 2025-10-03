7e1a1581572f86a57f5d8a593c90c2c566385dd7
CODECZERO <84948522+CODECZERO@users.noreply.github.com>
2025-10-03 16:55:23 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 // App.tsx
 import React from 'react';
 import { BrowserRouter as Router,  Routes } from 'react-router-dom';
+import { useKeepAlive } from './components/useKeepAlive.tsx';
 
 
 const App: React.FC = () => {
+  useKeepAlive(); // Initialize the keep-alive pinging
   return (
     <Router>
       <Routes>
